655a077bff04bb95a491734cbb2725723e27d7a6
ngoantt <ngoantt61125@gmail.com>
2016-04-02 12:29:18 +0700

    first commit

---
+++ app/src/main/java/ngoantran/com/farmapp/fragment/NewsFragment.java
@@ -0,0 +1,27 @@
+package ngoantran.com.farmapp.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ngoantran.com.farmapp.R;
+
+
+public class NewsFragment extends Fragment {
+
+    public NewsFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+}
